733fe8768ad359b32d9a93e780862afbda39429f
steven-marz <109651463+steven-marz@users.noreply.github.com>
2022-08-02 17:27:36 +0900

    Update WLM_Server.py

--- WLM_Server.py
+++ WLM_Server.py
@@ -10,12 +10,14 @@ import sys
 import pickle
 import numpy as np
 
+# Load in the DLL provided by HighFinesse
 DLL_PATH = "wlmData.dll"
 try:
     wlmData.LoadDLL(DLL_PATH)
 except:
     sys.exit("Error: Couldn't find DLL on path %s. Please check the DLL_PATH variable!" % DLL_PATH)
 
+# Specify the IP address and TCP port which will be used to host the server
 host = '192.168.1.56'
 port = 5353
 
@@ -26,14 +28,16 @@ wlmData.dll.SetSwitcherMode(1)
 Wavelength = 8*[0]
 # Initialize the interferometer list, zeroth entry serves as an identifier for the client
 Interferometer = 8*[[]]
-# Initialize the combined list which will be sent
+# Initialize the combined list which will be sent over the network
 to_send = [Wavelength, Interferometer]
 
+# Create a function which will manage the connection with the client
 def client_handler(connection):
+    # Loop to continually interact with the client
     while True:
         data = connection.recv(4096)
         selec_list = pickle.loads(data)
-        #print(selec_list)
+        
         for n in range(8):
             # Set the exposure times accoring to selec_list
             try:
@@ -51,6 +55,7 @@ def client_handler(connection):
                 else:
                     Wavelength[n] = f'{test_wavelength}'
                 to_send[0] = Wavelength
+            # Don't bother reading the wavelength if the client doesn't request it
             elif selec_list[n][0] == 'Off':
                 wlmData.dll.SetSwitcherSignalStates(n+1, 0, 0)
                 Wavelength[n] = '---'
@@ -64,15 +69,18 @@ def client_handler(connection):
                 wlmData.dll.GetPatternDataNum(n+1, wlmConst.cSignalAnalysisX, X)
                 Interferometer[n] = list(np.ctypeslib.as_array(X, (i//ii,)))
                 to_send[1] = Interferometer
+        # Send the acquired data
         connection.sendall(f'{len(pickle.dumps(to_send))}'.encode())
         time.sleep(0.5)
         connection.sendall(pickle.dumps(to_send))
 
+# Create a function which will connect to clients and assign these be managed in individual threads
 def accept_connections(ServerSocket):
     Client, address = ServerSocket.accept()
     print('Connected to: ' + address[0] + ':' + str(address[1]))
     threading.Thread(target=client_handler, args=(Client, )).start()
 
+# Lastly, create a function which starts the server
 def start_server(host, port):
     ServerSocket = socket.socket()
     try:
