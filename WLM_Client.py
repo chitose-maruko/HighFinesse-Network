a15532381d800d94df1a88a3f8561017e7d1c294
steven-marz <109651463+steven-marz@users.noreply.github.com>
2022-08-03 09:55:44 +0900

    Update WLM_Client.py

--- WLM_Client.py
+++ WLM_Client.py
@@ -24,44 +24,46 @@ try:
 except socket.error as e:
     print(str(e))
 
-# Define global variables which will reflect the desired mode and exposure time of each channel
+# Define global variable which will store the desired mode and exposure time of each channel
 # Starting values are Off with 1 ms exposure time
 selec_list = [['Off', '1'], ['Off', '1'], ['Off', '1'], ['Off', '1'], ['Off', '1'], ['Off', '1'], ['Off', '1'], ['Off', '1']]
 
-# Define another global variable to hold the target wavlengths, initialized to 0
+# Define another global variable to hold the target wavelengths, initialized to 0
 targets = [0, 0, 0, 0, 0, 0, 0, 0]
 
+# This class contains the function which will run in a thread separate from the GUI
+# and manages the server connection
 class Transmission(QtCore.QObject):
     # Create signal to send the data for plotting
     data = QtCore.pyqtSignal(list)
     
     # The function that manages the connection and updates variable with received data
     def update(self):
-        # Create list to store the wavelength-target data for plotting
+        # Create list to store the wavelength-target difference data for plotting
         wvl_longdata = [[], [], [], [], [], [], [], []]
     
         while True:
-            #print(selec_list[0])
-            # Pickles and sends selction list    
+            # Pickles and sends selection list    
             to_send = pickle.dumps(selec_list)
             ClientSocket.sendall(to_send)
             # Reads in the length of the message to be received
             length = ClientSocket.recv(8).decode()
-            # Reads data from server, stores in msg
+            
             msg = []
+	    # Reads data sent from the host, stores in msg
             while len(b"".join(msg)) < int(length):
                 temp = ClientSocket.recv(8192)
                 msg.append(temp)
             # Unpickle msg
             data = pickle.loads(b"".join(msg))
+
             # Store wavelength and interferometer data in separate lists
             wvl_data = data[0]
             int_data = data[1]
 
+	    # Conditionally calculate the difference between measured and target wavelength
             for i in range(8):
-                # Only do this step if the user wants the plot
                 if selec_list[i][0] == 'Wvl Difference' or selec_list[i][0] == 'Both Graphs':
-                    # Try to find difference between measured and target wavelength
                     try:
                         diff = float(wvl_data[i]) - float(targets[i])
                     except: 
@@ -73,9 +75,12 @@ class Transmission(QtCore.QObject):
                         wvl_longdata[i].pop(0)
                 else:
                     pass
-                    
+            # Send the data that has just been stored to another function for further operation        
             self.data.emit([int_data, wvl_longdata, wvl_data])
 
+	
+# This class sets up and runs the GUI, while using the Transmission class in a separate thread
+# to interact with the server
 class Window(QtGui.QWidget):
     # Create selectable modes for each channel
     modes = ['Off', 'Pause Graphs', 'Interferometer', 'Wvl Difference', 'Both Graphs']
@@ -83,20 +88,17 @@ class Window(QtGui.QWidget):
     # A list of RGB color codes to differentiate channels
     color = [(255,160,122),(238,232,170),(152,251,152),(72,209,204),(186,85,211),(255,192,203),(188,143,143),(220,20,60)]
     
-    # Create signals to send the selections of operation mode and exposure time
-    operation = QtCore.pyqtSignal(list)
-    exposure = QtCore.pyqtSignal(list)
-    
     def __init__(self, *args, **kwargs):
         super(Window, self).__init__(*args, **kwargs)
         
         self.setWindowTitle('Wavelength Meter')
         self.setGeometry(100, 100, 500, 750)
         self.setStyleSheet("background-color: black")
+	
         # Initialize widgets for...
         # Wavelength display
         self.wvl_lbl = 8*[0]
-        # Operation mode selection
+        # Operation mode selection menu
         self.menu_master = 8*[0]
         # Target wavelength entry
         self.tgt_master = 8*[0]
@@ -106,6 +108,7 @@ class Window(QtGui.QWidget):
         self.expo_master = 8*[0]
         # Label for exposure time entry
         expo_lbl = 8*[0]
+	
         # Loop over channels
         for i in range(8):
             self.wvl_lbl[i] = QtGui.QLabel('<h4>Channel ' + f'{i+1}' + '</h4>', parent=self)
@@ -125,11 +128,13 @@ class Window(QtGui.QWidget):
             expo_lbl[i].setStyleSheet('color: white')
 
         # Create the widgets for plotting
+	# Wavelength Difference plot
         self.wvl = pg.PlotWidget(parent=self)
         self.wvl.setTitle('Wavelength Difference')
         self.wvl.addLegend()
         self.wvl.showAxis('bottom', False)
         self.wvl.setLabel('left', 'nm')
+	# Interferometer plot
         self.inter = pg.PlotWidget(parent=self)
         self.inter.setTitle('Interferometer')
         self.inter.addLegend()
@@ -156,21 +161,26 @@ class Window(QtGui.QWidget):
 
         layout.addWidget(self.wvl, 6, 4, 3, 4)
         layout.addWidget(self.inter, 6, 0, 3, 4)
-            
+        
+	# Run the function, defined later in this code, which starts the thread for the Transmission class
         self.worker_thread()
            
+    # This function updates the GUI with data received from the server
     def gui_update(self, data):
-        
+        # Clear the plots whenever new data is received
         self.wvl.clear()
-        self.inter.clear()   
+        self.inter.clear() 
+	
         for i in range(8):
+	    # Use the channel labels to display current wavelength
             self.wvl_lbl[i].setText('<h4>Ch '+f'{i+1}: {data[2][i]} nm </h4>')
-            # Update selec_list '<h2>Channel ' + f'{i+1}' + '</h2>'
+
+            # Update global lists with user entered information
             selec_list[i][0] = self.menu_master[i].currentText()
             selec_list[i][1] = self.expo_master[i].text()
-            # Store entered target wavelengths
-            #print(data[0][0])
             targets[i] = self.tgt_master[i].text()
+
+	    # Plot according to user requests
             if selec_list[i][0] != 'Off':
                 try:
                     self.wvl.plot(data[1][i], name=f'Ch{i+1}', pen=self.color[i])
@@ -180,22 +190,22 @@ class Window(QtGui.QWidget):
                     self.inter.plot(data[0][i], name=f'Ch{i+1}', pen=self.color[i])
                 except:
                     pass
-                
+	
+    # This functions runs the transmission class in another thread       
     def worker_thread(self):
         self.thread = QtCore.QThread()
         self.worker = Transmission()
+	# Tie transmission class to the new thread
         self.worker.moveToThread(self.thread)
+	# Once the thread starts, run the update function in Transmission
         self.thread.started.connect(self.worker.update)
+	# Connect the data emitted from Transmission to the gui_update function
         self.worker.data.connect(self.gui_update)
         self.thread.start()
         
 
-# Set up GUI
+# Set up and run GUI
 app = QtGui.QApplication([])
-
-# Run the GUI
 window = Window()
 window.show()
-
-# Close the entire program once window closes
 sys.exit(app.exec_())
