561b3994f69d274c084844167310207d5a8c3908
Chitose Maruko <chitosemaruko@gmail.com>
2023-05-24 16:54:54 +0900

    Update WLM_Server.py
    Periodic calibration and multi-client operation is confirmed on this version. Laser locking has not been tested.

--- WLM_Server.py
+++ WLM_Server.py
@@ -59,11 +59,10 @@ Interferometer = 8 * [[]]
 Exposures=8*[1]
 PIDs=8*[[False,0,0,0]]
 Targets=8*["___"]
-#global list to keep track of which channel is in use
-#first element (which is also the list) represents the physical statust of the channel
-#as clients joins, lists that represents the use of the channel in each client will be concatenated
 Channels=[8*[False]]
-CAL = False
+# # Initialize the combined list which will be sent over the network
+# to_send = [Wavelength, Interferometer,Exposures,PIDs,Targets]
+
 #global variable to indicate the updates avaialbility for parameters
 update=True
 #Initialize the list of parameter update avaialbilities for each client
@@ -81,6 +80,8 @@ def client_handler(connection,counter):
     global Channels
     global Targets
     global initialize
+    global CAL
+    expoSkipped = False
     client_id = str(counter)
     exposures=[False,Exposures]
     pids =[False,PIDs]
@@ -89,33 +90,51 @@ def client_handler(connection,counter):
     if counter==0:
         initialize=True
     while True:
-        #initialize the boolean to flag the overwriting exposure value by the Highfinesse application
         exp_overwrite =False
-        #receive mesage from the client
         try: 
-            selec_list=recv_msg(connection,HEADERLENGTH)
+            length=int(connection.recv(8))
         except:
             break
-        #in case when there is no other client is running and this is the first time to loop,
-        #reflect all the user input of the given client as the valid selection
-        #without the flowwing lines all the clients' user parameter will be set to server default 
-        # (which defeats the purpose of loading previous config in client) or the user input is overwritten each time new client joins
+        msg=[]
+        while len(B"".join(msg))<length:
+            temp=connection.recv(64)
+            msg.append(temp)
+        selec_list = pickle.loads(b"".join(msg))
+        ch_active=[]
         if initialize:
             selec_list[-1]=[True,True,True]
             initialize=False
-        
-        #check which WLM chennel should be active according to the user input from the client and store the active channel number to the list
-        ch_active=check_chs_status(client_id)
 
-        #check if exposure was updated from the Highfinesse application side.
+        for i in range(8):
+            if selec_list[i][0]!= "Off":
+                ch_active.append(i+1)
+            elif Channels[0][i]:
+                Channels[int(client_id) +1][i]=False
+                ch_count =0
+                for ch in Channels:
+                    if ch[i]:
+                        ch_count+=1
+                if ch_count<2:
+                    # test.SetSwitcherSignalStates(i+1, 0, 0)
+                    wlmData.dll.SetSwitcherSignalStates(i+1, 0, 0)
+                    Channels[0][i]=False
+            else:
+                Channels[int(client_id) +1][i]=False
+
         for ch in ch_active:
             try:
+                #test.SetSwitcherSignalStates(ch, 1, 1)
+                Channels[int(client_id) +1][ch-1]=True
+                Channels[0][ch-1]=True
                 if not CAL:
-                    # test.SetSwitcherSignalStates(ch, 1, 1)
                     wlmData.dll.SetSwitcherSignalStates(ch, 1, 1)
-                    #exposure reading from the wavemeter itself
+
+                #exposure reading from the wavemeter itself
+                if not CAL:
                     expo_read=wlmData.dll.GetExposureNum(ch,1,0) 
-                    # expo_read = test.GetExposureNum(ch, 1,0)
+                # expo_read = test.GetExposureNum(ch, 1,0)
+
+                
 
                 if expo_read!=Exposures[ch-1]:
                     Exposures[ch-1]=expo_read
@@ -124,22 +143,39 @@ def client_handler(connection,counter):
                         client_dict[elm].updateExpo = True
             except: 
                 pass
-        #reflect the parameter updates from another client if there is any. but overwrite
+        #reflect the parameter updates from another client if there is any but overwrite
         # it if there is newer update
-        client.paramupdate(selec_list[-1])
-        
-        if selec_list[-1][0]==True:
+        if client.updateExpo and (not selec_list[-1][0]):
+            exposures[0]=True
+            exposures[1]=Exposures
+            client.updateExpo=False
+
+        if client.updatePID and (not selec_list[-1][1]):
+            pids[0]=True
+            pids[1]=PIDs
+            client.updatePID=False
+
+        if client.updateTgts and (not selec_list[-1][2]):
+            tgts[0]=True
+            tgts[1]=Targets
+
+        if selec_list[-1][0] or expoSkipped:
             for ch in ch_active:
             # Set the exposure times accoring to selec_list
                 try:
-                    if (CAL or exp_overwrite)==False:
-                        # # #line for local test
+                    if exp_overwrite==False:
+                        # #line for local test
                         # test.SetExposureNum(ch, 1, int(selec_list[ch-1][1]))
                         #line for machine test
-                        wlmData.dll.SetExposureNum(ch, 1, int(selec_list[ch-1][1]))
-                        Exposures[ch-1]=int(selec_list[ch-1][1])                  
+                        if CAL:
+                            expoSkipped =True
+                        else:
+                            wlmData.dll.SetExposureNum(ch, 1, int(selec_list[ch-1][1]))
+                            Exposures[i]=int(selec_list[ch-1][1])                  
                 except:
                     pass
+                if expoSkipped:
+                    expoSkipped=False
 
             selec_list[-1][0]=False
             for key in client_dict:
@@ -167,14 +203,15 @@ def client_handler(connection,counter):
         
         for ch in ch_active:
 
-           
-            if not CAL:
-                 # # Manage sending the wavelength data
-                # #line for the local test
-                # test_wavelength = test.GetWavelengthNum(ch, 0)
-                # Wavelength[ch-1] = f"{test_wavelength}"
-                
-                #line for the machine run
+            # # Manage sending the wavelength data
+            # #line for the local test
+            # test_wavelength = test.GetWavelengthNum(ch, 0)
+            # Wavelength[ch-1] = f"{test_wavelength}"
+
+            #line for the machine run
+            if CAL:
+                Wavelength[ch-1]="Calibrating..."
+            else:
                 test_wavelength = wlmData.dll.GetWavelengthNum(ch, 0)
                 if test_wavelength == wlmConst.ErrOutOfRange:
                     Wavelength[ch-1] = "Error: Out of Range"
@@ -182,23 +219,20 @@ def client_handler(connection,counter):
                     Wavelength[ch-1] = f"Error code: {test_wavelength}"
                 else:
                     Wavelength[ch-1] = f"{test_wavelength}"
-                #Manage sending the interferometer data
-                if (
-                    selec_list[ch-1][0] == "Interferometer"
-                    or selec_list[ch-1][0] == "Both Graphs"
-                ):
-                    # test.randomPattern(ch)
-                    # Interferometer[ch-1]=test.patternList[ch-1]
-                    #comment out the following block for the local test
-                    n = wlmData.dll.GetPatternItemCount(wlmConst.cSignal1Interferometers)
-                    nn = wlmData.dll.GetPatternItemSize(wlmConst.cSignal1Interferometers)
-                    wlmData.dll.SetPattern(
-                        wlmConst.cSignal1Interferometers, wlmConst.cPatternEnable
-                    )
-                    X = wlmData.dll.GetPatternNum(ch, wlmConst.cSignal1Interferometers)
-                    wlmData.dll.GetPatternDataNum(ch, wlmConst.cSignalAnalysisX, X)
-                    Interferometer[ch-1] = list(np.ctypeslib.as_array(X, (n // nn,)))
-        
+                # Manage sending the interferometer data
+            if (
+                    (selec_list[ch-1][0] == "Interferometer"
+                    or selec_list[ch-1][0] == "Both Graphs") and not CAL
+            ):
+                #comment out the following block for the local test
+                n = wlmData.dll.GetPatternItemCount(wlmConst.cSignal1Interferometers)
+                nn = wlmData.dll.GetPatternItemSize(wlmConst.cSignal1Interferometers)
+                wlmData.dll.SetPattern(
+                    wlmConst.cSignal1Interferometers, wlmConst.cPatternEnable
+                )
+                X = wlmData.dll.GetPatternNum(ch, wlmConst.cSignal1Interferometers)
+                wlmData.dll.GetPatternDataNum(ch, wlmConst.cSignalAnalysisX, X)
+                Interferometer[ch-1] = list(np.ctypeslib.as_array(X, (n // nn,)))
         
         to_send = [Wavelength, Interferometer,exposures,pids,tgts]
         # Send the acquired data
@@ -236,6 +270,7 @@ def PID_calc():
     global PIDs
     global Targets
     global offset
+    global CAL
     ti = time.perf_counter()+0
     tis = 8*[ti]
     tfs=8*[0.0]
@@ -248,24 +283,21 @@ def PID_calc():
     outputs=8*[0]
     print("PID operation started")
     while True:
-        if not CAL:
-            for i in range(8):
-                if PIDs[i][0]:
-                    try:
-                        #line for the machine run
-                        test_wavelength = wlmData.dll.GetWavelengthNum(i+1, 0)
-                        # #line for local test
-                        # test_wavelength=test.GetWavelengthNum(i+1, 0)
-                        if test_wavelength <= 100:
-                            print(f"Error code: {test_wavelength}")
-                        else:
-                            errors_current[i] = float(test_wavelength)-float(Targets[i])
+        for i in range(8):
+            if PIDs[i][0] and not CAL:
+                try:
+                    #line for the machine run
+                    test_wavelength = wlmData.dll.GetWavelengthNum(i+1, 0)
+                    if test_wavelength <= 100:
+                        print(f"Error code: {test_wavelength}")
+                    else:
+                        errors_current[i] = float(test_wavelength)-float(Targets[i])
 
-                    except:
-                        pass
-                    if PIDs[i][2]==0:
-                        integrals[i]=0
-                    
+                except:
+                    pass
+                if PIDs[i][2]==0:
+                    integrals[i]=0
+                if errors_current !=errors_prev:
                     tfs[i] = time.perf_counter()
                     dt = float(tfs[i]-tis[i])
                     dtTot+=dt
@@ -285,23 +317,24 @@ def PID_calc():
                         
                     except:
                         pass
-                    try:
-                        if pid_out < 4 and pid_out > -0.0285:
-                            output_PID(i+1,pid_out)
-                        elif pid_out >= 4:
-                            output_PID(i+1,4.0)
-                        else:
-                            output_PID(i+1,-0.0285)
-                        #print(f"Ch {i+1}: {selec_list[i][2]:.5f} V")
-                        outputs[i]=pid_out
-                        
-                    except:
-                        pass
-                            #print(f"Error in PID channel {i}")
-                else:
-                    offsets[i]= outputs[i]
-                    integrals[i]=0
-            errors_prev=errors_current
+
+                try:
+                    if pid_out < 4 and pid_out >= 0:
+                        output_PID(i+1,pid_out)
+                    elif pid_out >= 4:
+                        output_PID(i+1,4.0)
+                    else:
+                        output_PID(i+1,0)
+                    #print(f"Ch {i+1}: {selec_list[i][2]:.5f} V")
+                    outputs[i]=pid_out
+                    
+                except:
+                    pass
+                        #print(f"Error in PID channel {i}")
+            else:
+                offsets[i]= outputs[i]
+                integrals[i]=0
+        errors_prev=errors_current
 def output_PID(ch_num,vol_out):
     try:
         fpga_dac.dac(ch_num, vol_out)
@@ -311,12 +344,8 @@ def output_PID(ch_num,vol_out):
 
 # Lastly, create a function which starts the server
 def start_server(host, port):
-    global offset
     for i in range(8):
-        #line for local test
-        pass
-        #comment out for machine test
-        # output_PID(i+1,offset)
+        output_PID(i+1,offset)
     counter =0
     ServerSocket = socket.socket()
     try:
@@ -326,101 +355,42 @@ def start_server(host, port):
     print(f"Server is listening on TCP port {port}...")
     ServerSocket.listen()
     while True:
+        calThread=threading.Thread(target=calibrationHandler,args=())
+        calThread.start()
         accept_connections(ServerSocket,counter)
         Channels.append(8*[False])
         if counter==0:
-            output_PID(offset)
             thread_PID=threading.Thread(target=PID_calc, args=())
             thread_PID.daemon=True
             thread_PID.start()
         counter +=1
+def autocalibrate():
+    global CAL
+    CAL=True
+    try:
+        #pause all measurement before the calibration
+        wlmData.dll.Operation(wlmConst.cCtrlStopAll)
+        print('measurement paused for calibration')
+        calOut=wlmData.dll.Operation(wlmConst.cCtrlStartMeasurement)
+        if calOut==0:
+            print('calibration completed')
+        else:
+            print('calibration failed')
+    except:
+        pass
+    CAL=False
+def calibrationHandler():
+    autocalibrate()
+    calPeriod = 1*60 #calibration period in seconds
+    while True:
+        time.sleep(calPeriod)
+        autocalibrate()
 class ConnectionState():
     def __init__(self):
         self.updateExpo =False
         self.updatePID=False
         self.updateTgts=False
         self.options=8*["Off"]
-        self.exposures=[False,Exposures]
-        self.pids=[False,PIDs]
-        self.tgts=[False,Targets]
-    def paramUpdate(self,updList):
-        #reflect the parameter updates from another client if there is any. but overwrite
-        # it if there is newer update
-        if self.updateExpo and (not updList[0]):
-            self.exposures[0]=True
-            self.exposures[1]=Exposures
-            self.updateExpo=False
-
-        if self.updatePID and (not updList[0]):
-            self.pids[0]=True
-            self.pids[1]=PIDs
-            self.updatePID=False
-
-        if self.updateTgts and (not updList[0]):
-            self.tgts[0]=True
-            self.tgts[1]=Targets
+        self.test =False
 client_dict={}
 start_server(host, port)
-
-def calibrate():
-    global CAL
-    CAL=True
-    try:
-                    #pause all measurement before the calibration
-                    wlmData.dll.Operation(wlmConst.cCtrlStopAll)
-                    wlmData.dll.Calibrate(wlmConst.cHeNe633, wlmConst.cReturnWavelengthVac,632.99,8)
-                    wlmData.dll.Operation(wlmConst.cCtrlStartMeasurement)
-    except:
-        pass
-    CAL =False
-def calibrationHandler():
-    calPeriod=60 #calibration period in seconds
-    calibrate()
-    while True:
-        time.sleep(calPeriod)
-
-def recv_msg(conn,len_head):
-# a function to manage receiving message from clients. 
-# First element has to be a socket.client object of the given connection the client handler is handling.
-#the second argument is an integer which represents the predetermined header length for sending just the data size.
-
-#receive an information about the data size
-    length=int(conn.recv(len_head))
-#initialize the bin to store the received data
-    msg=[]
-#loop to concatenate the received data
-#it is a good practive to keep the data size of each communication to a small number to prevent a packet loss
-    while len(B"".join(msg))<length:
-        #a variable to temporalily store the received data
-        temp=conn.recv(128)
-        #concatenate the received data in the bin
-        msg.append(temp)
-    #conver the data in binary format to normal list and return the value
-    return  pickle.loads(b"".join(msg))
-
-def check_chs_status(id):
-    global Channels
-    #check which WLM chennel should be active according to the user input from the client and store the active channel number to the list
-    active=[]
-    for i in range(8):
-        if selec_list[i][0]!= "Off":
-            active.append(i+1)
-            #record that the given chennel is used by the given client
-            Channels[int(id) +1][ch-1]=True
-            #record that the given chnnel is used by at least one client
-            Channels[0][ch-1]=True
-        else:
-            Channels[int(id) +1][i]=False
-
-        if Channels[0][i]:
-            #record that the given chennel is used by the given client
-            ch_count =0
-            #check whether other clients are using the given channel
-            for ch in Channels:
-                if ch[i]:
-                    ch_count+=1
-            #if no client is using the given chennel, turn it off
-            if ch_count<2:
-                wlmData.dll.SetSwitcherSignalStates(i+1, 0, 0)
-                Channels[0][i]=False
-    return active
